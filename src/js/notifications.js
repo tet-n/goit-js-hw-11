b5d267313ef40cc93ee8efef464c2e6192e2210a
Tetiana Nivchyk <tata.02od@gmail.com>
2023-03-26 14:34:44 +0300

    add changes

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -1,7 +1,12 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 function warningNotification(message) {
-  Notify.failure(message);
+  Notify.failure(message, {
+    position: 'center-center',
+    failure: {
+      notiflixIconColor: 'rgba(255,255,255,1)',
+    },
+  });
 }
 
 function infoNotification(message) {
@@ -9,7 +14,13 @@ function infoNotification(message) {
 }
 
 function successNotification(message) {
-  Notify.success(message);
+  Notify.success(message, {
+    success: {
+      background: '#eed814',
+      textColor: '#000',
+      notiflixIconColor: 'rgba(1,116,26,0.8)',
+    },
+  });
 }
 
 export { warningNotification, infoNotification, successNotification };
